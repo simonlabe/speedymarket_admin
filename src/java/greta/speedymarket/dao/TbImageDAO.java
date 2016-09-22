6f0a1ee3bf396a23e080267f3d907d687056be53
Gaël PHILIPPE <contact@gael-philippe.fr>
2016-09-22 13:57:31 +0200

    TbImageDAO Correction et formatage (sauts de lignes en extra)

--- src/java/greta/speedymarket/dao/TbImageDAO.java
+++ src/java/greta/speedymarket/dao/TbImageDAO.java
@@ -1,39 +1,25 @@
 package greta.speedymarket.dao;
-
 import greta.speedymarket.model.TbImage;
-
 import greta.speedymarket.services.HibernateUtil;
-
 import java.util.ArrayList;
-
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
-
 import org.hibernate.Session;
 
 @ManagedBean(name = "TbImageDAO")
-
 public class TbImageDAO {
 
     public void save(TbImage image) {
-
         System.out.println("persist image");
-
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
         try {
-
             session.beginTransaction();
-
             session.save(image);
-
             session.getTransaction().commit();
 
         } catch (RuntimeException e) {
-
             e.printStackTrace();
-
             session.getTransaction().rollback();
 
         }
@@ -41,23 +27,16 @@ public class TbImageDAO {
     }
 
     public void update(TbImage image) {
-
         System.out.println("merge image " + image.getINom() + " - " + image.getILibelle());
-
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
         try {
-
             session.beginTransaction();
-
             session.update(image);
-
             session.getTransaction().commit();
 
         } catch (RuntimeException e) {
-
             e.printStackTrace();
-
             session.getTransaction().rollback();
 
         }
@@ -65,23 +44,16 @@ public class TbImageDAO {
     }
 
     public void remove(TbImage image) {
-
         System.out.println("Suppression de l'image " + image);
-
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
         try {
-
             session.beginTransaction();
-
             session.delete(image);
-
             session.getTransaction().commit();
 
         } catch (RuntimeException e) {
-
             e.printStackTrace();
-
             session.getTransaction().rollback();
 
         }
@@ -89,23 +61,16 @@ public class TbImageDAO {
     }
 
     public List<TbImage> findAll() {
-
         List<TbImage> lesImages = new ArrayList<>();
-
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
         try {
-
             session.beginTransaction();
-
             lesImages = session.createQuery("from TbImage").list();
-
             session.getTransaction().commit();
 
         } catch (RuntimeException e) {
-
             e.printStackTrace();
-
             session.getTransaction().rollback();
 
         }
